0c50f8dcc2bfd85ab9ee8543b4b60a7ac000b32d
sharath <sharathramku@umass.edu>
2018-03-03 23:00:15 -0500

    get reviews endpoint

---
+++ models/resp/Reviews.go
@@ -0,0 +1,19 @@
+package resp
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/sharath/opentutor/models/intern"
+	"gopkg.in/mgo.v2"
+)
+
+func Reviews(usr string, users *mgo.Collection, reviews *mgo.Collection) gin.H {
+	user, _ := intern.GetUser(usr, users)
+	var qry []*intern.Review
+	for _, id := range user.Reviews {
+		find, _ := intern.GetReview(id, reviews)
+		qry = append(qry, find)
+	}
+	return gin.H{
+		"reviews": qry,
+	}
+}
